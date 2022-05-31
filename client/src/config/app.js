9964734c46531fb5214124f5b3ff5a02ff4f4e9d
aryajf <aryajavasfatih888@gmail.com>
2022-05-31 09:10:25 +0700

    Finalizing v1

--- client/src/config/app.js
+++ client/src/config/app.js
@@ -1,11 +1,11 @@
 const module =  {
-    apiURL: 'http://localhost:3000/',
-    adminURL: 'http://localhost:8081',
-    baseURL: 'http://localhost:8080',
+    // apiURL: 'http://localhost:3000/',
+    // adminURL: 'http://localhost:8081',
+    // baseURL: 'http://localhost:8080',
 
-    // apiURL: 'https://berirezeki-api.digitalinteraktif.com/',
-    // adminURL: 'https://beri-rezeki-admin.netlify.app',
-    // baseURL: 'https://berirezeki.netlify.app',
+    apiURL: 'https://berirezeki-api.digitalinteraktif.com/',
+    adminURL: 'https://berirezeki-admin.netlify.app',
+    baseURL: 'https://berirezeki.netlify.app',
 }
 
 export default module
